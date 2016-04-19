4a0890e3a21bcd8360676c9f4d53e7adec458194
Roy Ronalds <tchalvakspam@gmail.com>
2016-04-19 16:16:26 -0400

    App: Allow crossdomain access to api.

--- app.py
+++ app.py
@@ -1,6 +1,55 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request, url_for
+from datetime import timedelta
+from flask import Flask, jsonify, abort, make_response, request, url_for, current_app
 from flask.ext.httpauth import HTTPBasicAuth
+from functools import update_wrapper
+
+''' Decorator to allow crossdomain access '''
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
+
 auth = HTTPBasicAuth()
 
 app = Flask(__name__)
@@ -41,11 +90,13 @@ def unauthorized():
     #Return 403 to prevent browsers from url prompting on unauth.
     return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
-@app.route('/chat/api/v1.0/chats', methods=['GET'])
+@app.route('/chat/api/v1.0/chats', methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def get_chats():
     return jsonify({'chats': [make_public_chat(chat) for chat in chats]})
 
-@app.route('/chat/api/v1.0/chats/<int:chat_id>', methods=['GET'])
+@app.route('/chat/api/v1.0/chats/<int:chat_id>', methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
 def get_chat(chat_id):
     chat = [chat for chat in chats if chat['id'] == chat_id]
     if len(chat) == 0:
@@ -57,6 +108,7 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/chat/api/v1.0/chats', methods=['POST'])
+@crossdomain(origin='*')
 @auth.login_required
 def create_chat():
     if not request.json or not 'title' in request.json:
